3e4ae8e5c14df3aaf5da3bf331bd7c9099ebd07b
IrynaVal <piv8050@gmail.com>
2023-03-29 23:32:05 +0300

    part2-toolkit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
-import { createStore } from 'redux';
-import { devToolsEnhancer } from '@redux-devtools/extension';
-import { rootReducer } from './reducer';
+// import { createStore } from 'redux';
+// import { devToolsEnhancer } from '@redux-devtools/extension';
+// import { rootReducer } from './reducer';
 
 // Початкове значення стану Redux для кореневого редюсера,
 // якщо не передати параметр preloadedState.
@@ -23,6 +23,16 @@ import { rootReducer } from './reducer';
 // };
 
 // Створюємо розширення стора, щоб додати інструменти розробника
-const enhancer = devToolsEnhancer();
+// const enhancer = devToolsEnhancer();
 
-export const store = createStore(rootReducer, enhancer);
+// export const store = createStore(rootReducer, enhancer);
+
+import { configureStore } from '@reduxjs/toolkit';
+import { tasksReducer, filtersReducer } from './reducer';
+
+export const store = configureStore({
+  reducer: {
+    tasks: tasksReducer,
+    filters: filtersReducer,
+  },
+});
